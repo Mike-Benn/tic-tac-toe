eefa313565c152acd5bfff2c63ddfc7f11097345
Mike-Benn <mbenn49@wgu.edu>
2024-01-24 05:47:13 -0500

    Create board square variables

---
+++ script.js
@@ -0,0 +1,27 @@
+const topLeft = document.querySelector('#top-left');
+const topMiddle = document.querySelector('#top-middle');
+const topRight = document.querySelector('#top-right');
+const middleLeft = document.querySelector('#middle-left');
+const middleMiddle = document.querySelector('#middle-middle');
+const middleRight = document.querySelector('#middle-right');
+const bottomLeft = document.querySelector('#bottom-left');
+const bottomMiddle = document.querySelector('#bottom-middle');
+const bottomRight = document.querySelector('#bottom-right');
+
+
+function createBoard () {
+    const board = [[null , null , null] , [null , null , null] , [null , null , null]];
+    return {board};
+}
+
+function createPlayer (name) {
+    return {name};
+}
+
+
+function gameStart () {
+    let board = createBoard();
+    
+}
+
+
