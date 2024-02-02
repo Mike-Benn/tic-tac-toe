ee1563ae50dd5af3260f3a8541ca8da4fd4b0b3f
Mike-Benn <mbenn49@wgu.edu>
2024-02-02 00:51:15 -0500

    Make constructors for board,player,cell

--- script.js
+++ script.js
@@ -7,60 +7,153 @@ const middleRight = document.querySelector('#middle-right');
 const bottomLeft = document.querySelector('#bottom-left');
 const bottomMiddle = document.querySelector('#bottom-middle');
 const bottomRight = document.querySelector('#bottom-right');
-const playerOne = document.querySelector('#player-one');
-const playerTwo = document.querySelector('#player-two');
-let board = [ [ [topLeft , "0 0"] , [topMiddle , "0 1"] , [topRight , "0 2"] ] , [ [middleLeft , "1 0"] , [middleMiddle , "1 1"] , [middleRight , "1 2"] ] , 
+const playerX = document.querySelector('#player-x');
+const playerO = document.querySelector('#player-o');
+const xImage = document.querySelector('#x-image');
+const oImage = document.querySelector('#o-image');
+const xText = document.querySelector('#x-text');
+const oText = document.querySelector('#o-text');
+const rootStyles = getComputedStyle(document.documentElement);
+const primaryColor = rootStyles.getPropertyValue('--primary-color');
+const secondaryColor = rootStyles.getPropertyValue('--secondary-color');
+const greyColor = rootStyles.getPropertyValue('--grey-color');
+const pinkColor = rootStyles.getPropertyValue('--pink-color');
+const orangeColor = rootStyles.getPropertyValue('--orange-color');
+
+
+let board = [ [ [topLeft , "0 0" , ] , [topMiddle , "0 1"] , [topRight , "0 2"] ] , [ [middleLeft , "1 0"] , [middleMiddle , "1 1"] , [middleRight , "1 2"] ] , 
 [ [bottomLeft , "2 0"] , [bottomMiddle , "2 1"] , [bottomRight , "2 2"] ] ];
 
-
-
-
-console.log(topLeft.classList.contains("gamesquare"));
-
-function createBoard() {
-    const board = [[null , null , null] , [null , null , null] , [null , null , null]];
-    return {board};
-}
-
-function createPlayer (name) {
-    return {name};
-}
-
-
-function gameStart() {
-    let board = createBoard();
-    
-}
   
-function boardSetupHelper(event) {
-    if (playerOne.classList.contains('player-active')) {
-        event.target.textContent = "X";
-        event.target.removeEventListener('click' , boardSetupHelper);
-        playerOne.classList.remove('player-active');
-        playerTwo.classList.add('player-active');
-    } else if (playerTwo.classList.contains('player-active')) {
-        event.target.textContent = "O";
-        event.target.removeEventListener('click' , boardSetupHelper);
-        playerTwo.classList.remove('player-active');
-        playerOne.classList.add('player-active');
+
+/*
+let top = {
+    
+    topLeft: {
+        value: null,
+        adjacent: [ [topMiddle , topRight] , [middleLeft , bottomLeft] , [middleMiddle , bottomRight]]
+    },
+
+    topMiddle: {
+        value: null,
+        adjacent: [ [topLeft , topRight] , [middleMiddle , bottomMiddle] ],
+        coordinates: "0 1"
+    },
+
+    topRight: {
+        value: null,
+        adjacent: [ [topMiddle , topLeft] , [middleMiddle , bottomLeft] , [middleRight , bottomRight] ],
+        coordinates: "0 2"
     }
 }
-console.log(board[0][0][0]);
+*/
 
+function GameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-function boardSetup() {
-    for (let x = 0; x < 3; x++) {
-        for(let y = 0; y < 3; y++) {
-            board[x][y][0].addEventListener('click' , boardSetupHelper);
+    for (let r = 0; r++; r < rows) {
+        board[r] = [];
+        for (let c = 0; c++; c < columns) {
+            board[r].push(Cell());
         }
     }
+
+    const getBoard = () => board;
+
+    const placeToken = (row , column , token) => {
+        if (board[row][column].getValue() === 0) {
+            board[row][column].addToken(token);
+        } else {
+            console.log("Invalid move");
+        }
+    }
+
+    const printBoard = () => {
+        const boardWithValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithValues);
+    }
+
+    return {
+        getBoard,
+        placeToken,
+        printBoard
+    };
+
+
+}
+
+function Player(name , token) {
+    const playerName = name;
+    const playerToken = token;
+
+    const getName = () => playerName;
+    const getToken = () => playerToken;
+
+    return {
+        getName,
+        getToken
+    };
+
+}
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (token) => {
+        value = token;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    };
 }
 
 
-boardSetup();
 
-// Gameboard array needs:
-// name, coordinates, necessary checks
+function turnSetupX() {
+    playerO.classList.remove('active');
+    oImage.src = "./images/oletter.png";
+    oImage.style.backgroundColor = secondaryColor;
+    playerO.style.backgroundColor = secondaryColor;
+    oText.style.color = greyColor;
+    oText.style.backgroundColor = secondaryColor;
+
+    playerX.classList.add('active');
+    xImage.src = "./images/xletter-inverted.png";
+    xImage.style.backgroundColor = greyColor;
+    playerX.style.backgroundColor = greyColor;
+    xText.style.color = secondaryColor;
+    xText.style.backgroundColor = greyColor;
+    
+}
+
+function turnSetupO() {
+    playerX.classList.remove('active');
+    xImage.src = "./images/xletter.png";
+    xImage.style.backgroundColor = secondaryColor;
+    playerX.style.backgroundColor = secondaryColor;
+    xText.style.color = greyColor;
+    xText.style.backgroundColor = secondaryColor;
+
+    playerO.classList.add('active');
+    oImage.src = "./images/oletter-inverted.png";
+    oImage.style.backgroundColor = greyColor;
+    playerO.style.backgroundColor = greyColor;
+    oText.style.color = secondaryColor;
+    oText.style.backgroundColor = greyColor;
+    
+}
+
+
+
+
+
+
 
 
 
